d4a483b3b07f0e321efe24340511bdc2faf02dce
Datla Rohit Varma <visaal1582892@gmail.com>
2025-01-06 23:44:10 +0530

    changed my paths of images and docs as required by vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html", "./public/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
